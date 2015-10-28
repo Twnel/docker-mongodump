07939396dc7c93ed4e24a7b1ffd4d1da1862a08e
Ericson Cepeda <hello.ericson@picorb.com>
2015-10-28 21:12:13 +0800

    Update backup.sh
    S3 upload

--- backup.sh
+++ backup.sh
@@ -10,5 +10,6 @@ FILE="/backup/backup-$DATE.tar.gz"
 mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT --db $DUMP_DB -u $MONGODB_ADMINUSERNAME -p $MONGODB_ADMINPASSWORD --authenticationDatabase admin
 tar -zcvf $FILE dump/
 rm -rf dump/
+aws s3 cp ${FILE} s3://${S3_BUCKET} 
 
 echo "Job finished: $(date)"
