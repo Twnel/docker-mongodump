18bdf89a9f3c6e9800df232afaf29d831bff1a6c
Ericson Cepeda <hello.ericson@picorb.com>
2015-10-28 20:36:50 +0800

    Update backup.sh
    Credentials

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ echo "Job started: $(date)"
 DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/backup/backup-$DATE.tar.gz"
 
-mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT
+mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT --db $DUMP_DB -u $MONGODB_ADMINUSERNAME -p $MONGODB_ADMINPASSWORD --authenticationDatabase admin
 tar -zcvf $FILE dump/
 rm -rf dump/
 
