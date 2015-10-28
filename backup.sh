cfd5929f4397299838db58849a9cddb6415098bd
Ericson Cepeda <hello.ericson@picorb.com>
2015-10-28 21:18:43 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ set -e
 echo "Job started: $(date)"
 
 DATE=$(date +%Y%m%d_%H%M%S)
-FILE="/backup/backup-$DATE.tar.gz"
+FILE="/backup/backup-$PREFIX-$DATE.tar.gz"
 
 mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT --db $DUMP_DB -u $MONGODB_ADMINUSERNAME -p $MONGODB_ADMINPASSWORD --authenticationDatabase admin
 tar -zcvf $FILE dump/
