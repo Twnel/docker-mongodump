12f17d044d9a19589b8cd2a1c89ab4907313170f
Tech-Twnel <tech@twnel.com>
2015-12-02 23:33:56 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ echo "Job started: $(date)"
 DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/backup/backup-$PREFIX-$DATE.tar.gz"
 
-mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR --port $MONGO_PORT_27017_TCP_PORT --db $DUMP_DB -u $MONGODB_ADMINUSERNAME -p $MONGODB_ADMINPASSWORD --authenticationDatabase admin
+mongodump --quiet --host $MONGO_PORT_27017_TCP_ADDR --port $MONGO_PORT_27017_TCP_PORT --db $DUMP_DB -u $MONGODB_ADMINUSERNAME -p $MONGODB_ADMINPASSWORD --authenticationDatabase admin
 tar -zcvf $FILE dump/
 rm -rf dump/
 aws s3 cp ${FILE} s3://${S3_BUCKET} 
