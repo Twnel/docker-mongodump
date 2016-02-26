156e4e50b68651e2416bea346d0d28a7f84ffa94
Ericson Cepeda <hello.ericson@picorb.com>
2016-02-26 21:13:33 +0800

    [update] create latest dump.

--- backup.sh
+++ backup.sh
@@ -6,11 +6,13 @@ echo "Job started: $(date)"
 
 DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/backup/backup-$PREFIX-$DATE.tar.gz"
+LATEST="/latest-$PREFIX.tar.gz"
 
 mongodump --quiet --host $MONGO_PORT_27017_TCP_ADDR --port $MONGO_PORT_27017_TCP_PORT --db $DUMP_DB -u $MONGODB_ADMINUSERNAME -p $MONGODB_ADMINPASSWORD --authenticationDatabase admin
 tar -zcvf $FILE dump/
 rm -rf dump/
-aws s3 cp ${FILE} s3://${S3_BUCKET} 
+aws s3 cp ${FILE} s3://${S3_BUCKET}
+aws s3 cp ${FILE} s3://${S3_BUCKET}${LATEST}
 curl --data "S3 => $S3_BUCKET: $FILE" "https://${SLACK_TEAM}.slack.com/services/hooks/slackbot?token=${SLACK_TOKEN}&channel=%23${SLACK_CHANNEL}"
 echo "Job finished: $(date)"
 exit 0
