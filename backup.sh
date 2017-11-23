6c2de8d2a63ff1c3b4d766e43d386cad54938c46
Ericson Cepeda <ericson@picorb.com>
2017-11-23 20:42:04 +0100

    [update] notify when backup begins.

--- backup.sh
+++ backup.sh
@@ -8,6 +8,7 @@ DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/backup/backup-$PREFIX-$DATE.tar.gz"
 LATEST="/latest-$PREFIX.tar.gz"
 
+curl --data "S3 => $LATEST: started" "https://${SLACK_TEAM}.slack.com/services/hooks/slackbot?token=${SLACK_TOKEN}&channel=%23${SLACK_CHANNEL}"
 mongodump --quiet --host $MONGO_PORT_27017_TCP_ADDR --port $MONGO_PORT_27017_TCP_PORT --db $DUMP_DB -u $MONGODB_ADMINUSERNAME -p $MONGODB_ADMINPASSWORD --authenticationDatabase admin
 tar -zcvf $FILE dump/
 rm -rf dump/
